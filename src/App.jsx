5fcc85fdded0e921fde1e4908bbc3bb26ad16932
Yash-Vashishth <imyashvashishth05@gmail.com>
2024-10-26 01:20:52 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,21 @@ import Footer from './components/Footer'
 import LocomotiveScroll from 'locomotive-scroll';
 
 const App = () => {
+  React.useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: document.querySelector('[data-scroll-container]'),
+      smooth: true
+    });
+  }, []);
+  return (
+    <div data-scroll-container>
+      <LandingPage />
+      <Marquee />
+      <About />
+      <Eyes />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
